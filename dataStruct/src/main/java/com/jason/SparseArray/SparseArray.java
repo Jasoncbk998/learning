3fee07afd92919b2c292bf040f8107129542fd5e
jasoncbk998 <jasoncbk998@163.com>
2020-07-17 14:11:12 +0800

    数据结构 稀疏数组&队列

---
+++ dataStruct/src/main/java/com/jason/SparseArray/SparseArray.java
@@ -0,0 +1,137 @@
+package com.jason.SparseArray;
+
+/**
+ * ClassName SparseArray
+ * Description
+ * Create by Jason
+ * Date 2020/7/17 12:49
+ *
+ * 优点：
+ *
+ * 避免了基本数据类型的装箱操作
+ * 不需要额外的结构体，单个元素的存储成本更低
+ * 数据量小的情况下，随机访问的效率更高
+ * 缺点：
+ *
+ * 插入操作需要复制数组，增删效率降低
+ * 数据量巨大时，复制数组成本巨大，gc()成本也巨大
+ * 数据量巨大时，查询效率也会明显下降
+ *
+ */
+public class SparseArray {
+    public static void main(String[] args) {
+        //创建一个原始的二维数组   11*11
+
+        // 0: 表示没有棋子,  1表示黑子    2 表示蓝子
+
+
+        int[][] chessArr1 = new int[11][11];
+
+
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+        chessArr1[4][5] = 2;
+        //输出
+        System.out.println("输出原始二维数组");
+        for (int[] row : chessArr1) {
+            for (int data : row) {
+                System.out.printf("%d \t ", data);
+            }
+            System.out.println();
+        }
+        /**
+         * 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 1 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 2 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 0 	 0 	 2 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         * 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0 	 0
+         */
+
+        //二維数组转化成为稀疏数组
+        // 1.遍历二维数组,得到非0数据的个数
+        int sum = 0;
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+
+
+        //2 创建对应的稀疏数组
+        int[][] sparseArr = new int[sum + 1][3];
+        //给稀疏数组赋值
+        sparseArr[0][0] = 11;
+        sparseArr[0][1] = 11;
+        sparseArr[0][2] = sum;
+        //遍历二维数组,将非0的值存到sparseArr中
+        int count = 0;//记录第几个非0的数据
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr1[i][j] != 0) {
+                    count++;
+                    sparseArr[count][0] = i;
+                    sparseArr[count][1] = j;
+                    sparseArr[count][2] = chessArr1[i][j];
+                }
+            }
+        }
+        //输出稀疏数组
+        System.out.println();
+        System.out.println("输出稀疏数组");
+        for (int i = 0; i < sparseArr.length; i++) {
+            System.out.printf("%d\t%d\t%d\t\n", sparseArr[i][0], sparseArr[i][1], sparseArr[i][2]);
+        }
+        /**
+         * 11	11	3
+         * 1	2	1
+         * 2	3	2
+         * 4	5	2
+         */
+
+
+        //稀疏数组  -> 二维数组
+        /**
+         * 首先 读取稀疏数组的第一行,根据第一行的数据,创建原始的二维数组
+         *
+         * 在读取稀疏数组后几行的数据,并赋值给原始的二维数组即可
+         */
+        int[][] chessArr2 = new int[sparseArr[0][0]][sparseArr[0][1]];
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+        //输出恢复后的二维数组
+        System.out.println();
+        System.out.println("恢复后的二维数组");
+        for(int[] row : chessArr2){
+            for(int data: row){
+                System.out.printf("%d\t",data);
+            }
+            System.out.println();
+        }
+        /**
+         * 0	0	0	0	0	0	0	0	0	0	0
+         * 0	0	1	0	0	0	0	0	0	0	0
+         * 0	0	0	2	0	0	0	0	0	0	0
+         * 0	0	0	0	0	0	0	0	0	0	0
+         * 0	0	0	0	0	2	0	0	0	0	0
+         * 0	0	0	0	0	0	0	0	0	0	0
+         * 0	0	0	0	0	0	0	0	0	0	0
+         * 0	0	0	0	0	0	0	0	0	0	0
+         * 0	0	0	0	0	0	0	0	0	0	0
+         * 0	0	0	0	0	0	0	0	0	0	0
+         * 0	0	0	0	0	0	0	0	0	0	0
+         */
+
+
+
+
+    }
+}
---
+++ dataStruct/src/main/java/com/jason/queue/ArrayQueueDemo.java
@@ -0,0 +1,138 @@
+package com.jason.queue;
+
+
+import java.util.Scanner;
+
+/**
+ * ClassName ArrayQueueDemo
+ * Description
+ * Create by Jason
+ * Date 2020/7/17 13:14
+ */
+public class ArrayQueueDemo {
+
+
+    public static void main(String[] args) {
+
+        //测试
+        //创建一个队列
+
+        ArrayQueue queue = new ArrayQueue(3);
+
+        char key = ' ';//接受用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输出一个菜单
+        while (loop) {
+            System.out.println("s(show): 显示队列");
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            System.out.println("h(head): 查看队列头的数据");
+            key = scanner.next().charAt(0);//接收一个字符
+
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a'://取出数据
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g':
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("取出的数据是%d\n", res);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 'h'://查看队列头的数据
+                    try {
+                        int res = queue.headQueue();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 'e'://退出
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        System.out.println("程序退出");
+
+    }
+
+
+}
+
+class ArrayQueue {
+    private int maxSize; // 表示数组的最大容量
+    private int front; // 队列头
+    private int rear; // 队列尾
+    private int[] arr; // 该数据用于存放数据, 模拟队列
+
+    public ArrayQueue(int arrMaxSize) {
+        maxSize = arrMaxSize;
+        arr = new int[maxSize];
+        front = -1; // 指向队列头部，分析出front是指向队列头的前一个位置.
+        rear = -1; // 指向队列尾，指向队列尾的数据(即就是队列最后一个数据)
+    }
+
+    public void showQueue() {
+//遍历
+        if (isEmpty()) {
+            System.out.println("队列是空的,没有数据");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d]\n", i, arr[i]);
+        }
+    }
+
+
+    //判断队列是否为空
+    private boolean isEmpty() {
+        return rear == front;
+
+    }
+
+    //获取队列的数据,出队列
+    public int getQueue() {
+        //判断队列是否是空
+        if (isEmpty()) {
+            throw new RuntimeException("队列空,不能获取数据");
+        }
+        front++;//front后移
+        return arr[front];
+    }
+
+    //添加数据到队列
+    public void addQueue(int n) {
+        //判断队列是否满
+        if (isFull()) {
+            System.out.println("队列满,不能加入数据");
+            return;
+        }
+        rear++; // 让rear后移
+        arr[rear] = n;
+    }
+
+
+    private boolean isFull() {
+        return rear == maxSize - 1;
+    }
+
+    //显示队列的头数据  注意不是取出数据
+    public int headQueue() {
+        if (isEmpty()) {
+            throw new RuntimeException("队列空的，没有数据~~");
+        }
+        return arr[front + 1];
+    }
+}
