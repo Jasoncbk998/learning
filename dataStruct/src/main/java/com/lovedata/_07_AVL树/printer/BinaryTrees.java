cc1f73c9dbe71c8a36a73980a2f399f5ff35de30
jasoncbk998 <jasoncbk998@163.com>
2020-08-27 15:48:46 +0800

    0827-02

---
+++ dataStruct/src/main/java/com/lovedata/_07_AVL树/printer/BinaryTrees.java
@@ -0,0 +1,53 @@
+package com.lovedata._07_AVL树.printer;
+
+import com.lovedata._06_二叉搜索树_重构.printer.BinaryTreeInfo;
+import com.lovedata._06_二叉搜索树_重构.printer.InorderPrinter;
+import com.lovedata._06_二叉搜索树_重构.printer.LevelOrderPrinter;
+import com.lovedata._06_二叉搜索树_重构.printer.Printer;
+
+/**
+ * 
+ * @author MJ Lee
+ *
+ */
+public final class BinaryTrees {
+
+	private BinaryTrees() {
+	}
+
+	public static void print(BinaryTreeInfo tree) {
+		print(tree, null);
+	}
+
+	public static void println(BinaryTreeInfo tree) {
+		println(tree, null);
+	}
+
+	public static void print(BinaryTreeInfo tree, PrintStyle style) {
+		if (tree == null || tree.root() == null) return;
+		printer(tree, style).print();
+	}
+
+	public static void println(BinaryTreeInfo tree, PrintStyle style) {
+		if (tree == null || tree.root() == null) return;
+		printer(tree, style).println();
+	}
+
+	public static String printString(BinaryTreeInfo tree) {
+		return printString(tree, null);
+	}
+
+	public static String printString(BinaryTreeInfo tree, PrintStyle style) {
+		if (tree == null || tree.root() == null) return null;
+		return printer(tree, style).printString();
+	}
+
+	private static Printer printer(BinaryTreeInfo tree, PrintStyle style) {
+		if (style == PrintStyle.INORDER) return new InorderPrinter(tree);
+		return new LevelOrderPrinter(tree);
+	}
+
+	public enum PrintStyle {
+		LEVEL_ORDER, INORDER
+	}
+}
