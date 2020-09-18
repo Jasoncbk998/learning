d2b6897ae2c53733eceb06d83dbce2a247e8600a
jasoncbk <jasoncbk998@163.com>
2020-09-18 10:08:37 +0800

    0918更新

---
+++ dataStruct/src/main/java/com/lovedata/question/_1_两数之和.java
@@ -0,0 +1,48 @@
+package com.lovedata.question;
+
+import com.lovedata.base._11_哈希表.map.HashMap;
+
+import java.util.Arrays;
+
+/**
+ * @Classname _1_两数之和
+ * @Description TODO
+ * @Date 2020/9/18 9:25 上午
+ * @Created by jason
+ */
+public class _1_两数之和 {
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * <p>
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * <p>
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int[] solution = solution(nums, 18);
+        System.out.println(Arrays.toString(solution));
+    }
+
+    private static int[] solution(int[] nums, int target) {
+        int[] ints = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int source = target - nums[i];
+            if (map.containsKey(source)) {
+                return new int[]{map.get(source), i};
+            }
+            map.put(nums[i],i);
+        }
+
+
+        return null;
+    }
+
+
+}
---
+++ dataStruct/src/main/java/com/lovedata/question/_5_最长回文子串.java
@@ -0,0 +1,29 @@
+package com.lovedata.question;
+
+/**
+ * @Classname _5_最长回文子串
+ * @Description TODO
+ * @Date 2020/9/18 9:47 上午
+ * @Created by jason
+ *
+ * 不会,题目总结中有答案,这个比较难,先放下
+ */
+public class _5_最长回文子串 {
+    public static void main(String[] args) {
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 示例 1：
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ * 输入: "cbbd"
+ * 输出: "bb"
+ */
+
+
+
+
+    }
+
+}
