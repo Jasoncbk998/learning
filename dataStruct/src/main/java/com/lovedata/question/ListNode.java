1e2f10b63a102fd8982e24929f7360c727fd4cb9
jasoncbk <jasoncbk998@163.com>
2020-09-19 10:30:12 +0800

    0919更新

--- dataStruct/src/main/java/com/lovedata/question/ListNode.java
+++ dataStruct/src/main/java/com/lovedata/question/ListNode.java
@@ -5,12 +5,51 @@ package com.lovedata.question;
  * Description
  * Create by Jason
  * Date 2020/9/18 10:15
+ *
+ *         ListNode l1 = new ListNode(2);
+ *         ListNode l2 = new ListNode(4);
+ *         ListNode l3 = new ListNode(3);
+ *         l1.setNext(l2);
+ *         l2.setNext(l3);
+ *         System.out.println(l1);
+ *         ListNode r1 = new ListNode(5);
+ *         ListNode r2 = new ListNode(6);
+ *         ListNode r3 = new ListNode(4);
  */
 public class ListNode {
     int val;
     ListNode next;
 
+    public ListNode() {
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
     ListNode(int x) {
         val = x;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return val + " -> " + next;
     }
 }
--- dataStruct/src/main/java/com/lovedata/question/_206_反转链表.java
+++ dataStruct/src/main/java/com/lovedata/question/_206_反转链表.java
@@ -18,8 +18,7 @@ public class _206_反转链表 {
      */
     public static void main(String[] args) {
 
-        ListNode node = new ListNode(1);
-        ListNode next = node.next;
+
     }
 
     public ListNode reverseList1(ListNode head) {
